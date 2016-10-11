aeb6875a4fe7597c72770d5c8c6827056bcdaf65
rulotico <rulotico@gmail.com>
2016-10-10 20:33:25 -0500

    someupdates

--- app/faq/faq.js
+++ app/faq/faq.js
@@ -18,25 +18,25 @@
 
     var dudas = [{
       img:'icon1',
-      titulo:'¿Qué es un hackatón?',
+      titulo:'¿QuÉ́ es un hackathon?',
       respuesta:'Un hackatón es un evento donde se reunen algunas de las personas más talentosas con diferentes perfiles para desarrollar un proyecto en 24 hasta 36 horas seguidas de programación. Al final se presentan los demos de las aplicaciones y se elige a los mejores.',
       img2:'icon5',
-      titulo2:'¿Qué pasa si no se programar?',
+      titulo2:'¿QuÉ́ pasa si no se programar?',
       respuesta2:'¡No hay problema! Es completamente irrelevante la experiencia que tengas dentro del hackathon, se trata más acerca de tu interés en la tecnología.',
     },{
       img:'icon2',
       titulo:'¿Si soy foráneo Mx Hacks me puede apoyar con mi viaje?',
       respuesta:'Sí, lo único que tienes que hacer es aplicar por una beca para poder venir a la CDMX saliendo de Guadalajara o Monterrey.',
       img2:'icon10',
-      titulo2:'Me acabo de graduar, ¿Aún puedo asistir a un evento?',
+      titulo2:'Me acabo de graduar, ¿AÚN puedo asistir a un evento?',
       respuesta2:'Sí. Cualquier persona que se haya graduado dentro de los últimos 12 meses aún puede asistir a un evento miembro de MLH. Después de eso, existen muchos hackathons no estudiantiles a los que puedes considerar asistir.',
     },{
       img:'icon4',
-      titulo:'¿Será molesto para la gente si les hago preguntas?',
+      titulo:'¿SeRá molesto para la gente si les hago preguntas?',
       respuesta:'¡Para Nada! Este es un malentendido muy común acerca de los hackathons y de hecho, es completamente lo opuesto. El ambiente en estos eventos está diseñado especialmente para alentar a todos a divertirse y ayudarse mutuamente. Si alguna vez te encuentras en problemas con algo, anda y busca a un voluntario que pueda ayudar y si no puede, seguramente conocerá a alguien que sí pueda.',
       img2:'icon9',
-      titulo2:'¿Habrá comida?',
-      respuesta2:'¡Por supuesto!. Tus comidas para el fin de semana serán completamente gratuitas, además muchos bocadillos y bebidas durante el evento para mantenerte con energía. Si tienes requisitos dietéticos específicos, deberás de especificarlo en tu aplicación, así podremos tomar las medidas necesarias.',
+      titulo2:'¿HabrÁ comida?',
+      respuesta2:'¡Por supuesto! Tus comidas para el fin de semana serán completamente gratuitas, además muchos bocadillos y bebidas durante el evento para mantenerte con energía. Si tienes requisitos dietéticos específicos, deberás de especificarlo en tu aplicación, así podremos tomar las medidas necesarias.',
     },{
       img:'icon7',
       titulo:'¿Quién puede participar?',
@@ -50,6 +50,6 @@
       respuesta:'Deberás contar con tu credencial de estudiante, tu laptop con su cargador, cargador de celular, gadgets y todo lo que necesites para estimular tu creatividad. Recuerda que está prohibido entrar con armas, drogas y/o alcohol.',
       img2:'icon11',
       titulo2:'¿Habrá premios?',
-      respuesta2:'Sí, pero recuerda que no sólo se trata de lo que puedes ganar sino también de lo que aprendas y construyas en 36 horas para demostrar que en México también estamos generando un gran avance en creación de tecnología . Al final del evento habrá un panel de jueces quienes juzgarán los proyectos y elegirán a los tres mejores basado en la creatividad, dificultad técnica, diseño y aplicabilidad.',
+      respuesta2:'Sí, pero recuerda que no sólo se trata de lo que puedes ganar sino también de lo que aprendas y construyas en 36 horas para demostrar que en México también estamos generando un gran avance en creación de tecnología. Al final del evento habrá un panel de jueces quienes juzgarán los proyectos y elegirán a los tres mejores basado en la creatividad, dificultad técnica, diseño y aplicabilidad.',
     }]
 })();
